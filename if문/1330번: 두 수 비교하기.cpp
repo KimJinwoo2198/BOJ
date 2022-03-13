fb6d3c0fc14de449c096db009d7b0dfd6c046a8b
battlebot <kimjw2198@gmail.com>
2022-03-13 21:28:12 +0900

    Delete 1330번: 두 수 비교하기.cpp

--- if문/1330번: 두 수 비교하기.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a,b;
-    scanf("%d %d", &a,&b);
-    if ( a < b ) printf("<");
-    else if ( a > b ) printf(">");
-    else if ( a == b ) printf("==");
-}
